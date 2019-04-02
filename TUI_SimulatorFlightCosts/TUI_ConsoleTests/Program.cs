d8d1eb94ba1328a280f509bda7c05a7a5248f019
Philippe Gung <philippe.gung@youscribe.com>
2019-04-02 17:34:20 +0200

    init

---
+++ TUI_SimulatorFlightCosts/TUI_ConsoleTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using GeoCoordinatePortable;
+using System;
+
+namespace TUI_ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            var londonAirportGPS = new GeoCoordinate(51.5048, 0.052745500000014545);
+            var charlesDeGaulesAeroportGPS = new GeoCoordinate(49.007, 2.559790000000021);
+
+            Console.WriteLine($"Distance aéroport : {londonAirportGPS.GetDistanceTo(charlesDeGaulesAeroportGPS) / 1000} km");
+            Console.ReadKey();
+        }
+    }
+}
