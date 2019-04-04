b23eda7c452de3c4f3e62a4c246301f1238fc9aa
Philippe Gung <philippe.gung@youscribe.com>
2019-04-04 12:29:38 +0200

    ajout acceptance test

---
+++ TUI_SimulatorFlightCosts/TUI_SimulatorFlightCosts_AcceptanceTests/Register_A_New_Flight_Steps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TUI_SimulatorFlightCosts_AcceptanceTests
+{
+    [Binding]
+    public class Register_A_New_FlightSteps
+    {
+        [When(@"A user enters a new flight called '(.*)' on system")]
+        public void WhenAUserEntersANewFlightCalledOnSystem(string flightName)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"The flight called '(.*)' can be retrived on system")]
+        public void ThenTheFlightCalledCanBeRetrivedOnSystem(string flightName)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
