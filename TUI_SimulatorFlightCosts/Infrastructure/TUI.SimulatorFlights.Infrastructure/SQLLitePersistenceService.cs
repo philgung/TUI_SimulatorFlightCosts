31a3f729b134e18f225d730d9cb2ddd6aa4c9f3a
Philippe Gung <philippe.gung@youscribe.com>
2019-04-05 17:56:45 +0200

    script de creation de table

--- TUI_SimulatorFlightCosts/Infrastructure/TUI.SimulatorFlights.Infrastructure/SQLLitePersistenceService.cs
+++ TUI_SimulatorFlightCosts/Infrastructure/TUI.SimulatorFlights.Infrastructure/SQLLitePersistenceService.cs
@@ -43,7 +43,7 @@ namespace TUI.SimulatorFlights.Infrastructure
             {
                 connection.Open();
                 command.CommandText = 
-                    "CREATE TABLE flight (id INT AUTOINCREMENT, " +
+                    "CREATE TABLE flight (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "flight_name VARCHAR(255), " +
                     "fuel_consumption_per_distance INT," +
                     "fuel_consumption_takeoff_effort INT," +
@@ -51,8 +51,8 @@ namespace TUI.SimulatorFlights.Infrastructure
                     "departure_airport_latitude double," +
                     "departure_airport_longitude double," +
                     "destination_airport_name VARCHAR(255)," +
-                    "departure_airport_latitude double," +
-                    "departure_airport_longitude double)"
+                    "destination_airport_latitude double," +
+                    "destination_airport_longitude double)"
                     ;
                 command.ExecuteNonQuery();
             }
---
+++ TUI_SimulatorFlightCosts/Presentation/TUI.SimulatorFlights.InitDBSqlite/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TUI.SimulatorFlights.Infrastructure;
+
+namespace TUI.SimulatorFlights.InitDBSqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new SQLLitePersistenceService();
+            Console.WriteLine("SQLite : Initialize table");
+            service.InitializeTables();
+            Console.WriteLine("SQLite : Tables were created.");
+        }
+    }
+}
