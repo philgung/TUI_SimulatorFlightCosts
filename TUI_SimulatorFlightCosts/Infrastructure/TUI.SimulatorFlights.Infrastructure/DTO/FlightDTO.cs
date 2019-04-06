5bf079d56f19a6520c5c78e05059dff8efa520ad
Philippe Gung <philippe.gung@youscribe.com>
2019-04-06 22:01:35 +0200

    persistence flightDTO !

---
+++ TUI_SimulatorFlightCosts/Infrastructure/TUI.SimulatorFlights.Infrastructure/DTO/FlightDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TUI.SimulatorFlights.Infrastructure.DTO
+{
+    public class FlightDTO
+    {
+        public string flight_name { get; set; }
+        public int fuel_consumption_per_distance { get; set; }
+        public int fuel_consumption_takeoff_effort { get; set; }
+        public string departure_airport_name { get; set; }
+        public double departure_airport_latitude { get; set; }
+        public double departure_airport_longitude { get; set; }
+        public string destination_airport_name { get; set; }
+        public double destination_airport_latitude { get; set; }
+        public double destination_airport_longitude { get; set; }
+    }
+}
--- TUI_SimulatorFlightCosts/Infrastructure/TUI.SimulatorFlights.Infrastructure/SQLLitePersistenceService.cs
+++ TUI_SimulatorFlightCosts/Infrastructure/TUI.SimulatorFlights.Infrastructure/SQLLitePersistenceService.cs
@@ -1,21 +1,23 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Data.SQLite;
 using TUI.Domain.SimulatorFlights;
 using TUI.SimulatorFlights.Domain;
+using TUI.SimulatorFlights.Infrastructure.DTO;
 
 namespace TUI.SimulatorFlights.Infrastructure
 {
     public class SQLLitePersistenceService : IPersistenceService
     {
-        private string _connectionString = "Data Source=TUI.SimulatorFlights.sqlite";
+        private string _connectionString = @"Data Source=..\..\..\..\Sqlite\TUI.SimulatorFlights.sqlite;Version=3;";
         public IFlight GetFlight(string flightName)
         {
             using (var connection = new SQLiteConnection(_connectionString))
             using (var command = connection.CreateCommand())
             {
                 connection.Open();
-                command.CommandText = "";
-                command.ExecuteNonQuery();
+                var flightDTO = connection.Query<FlightDTO>($"select * from flight where flight_name='{flightName}'");
+                
                 
             }
             
@@ -28,23 +30,26 @@ namespace TUI.SimulatorFlights.Infrastructure
             using (var command = connection.CreateCommand())
             {
                 connection.Open();
-                command.CommandText = "INSERT INTO flight (name) VALUES ('', '')";
+                command.CommandText = "INSERT INTO flight (flight_name,fuel_consumption_per_distance,fuel_consumption_takeoff_effort," +
+                    "departure_airport_name, departure_airport_latitude, departure_airport_longitude," +
+                    "destination_airport_name, destination_airport_latitude, destination_airport_longitude" +
+                   $") VALUES ('{flight.Name}', {flight.FuelConsumptionPerDistance}, {flight.FuelConsumptionTakeoffEffort},'departure_airport',{flight.DepartureAirport.GPSPosition.Latitude}, {flight.DepartureAirport.GPSPosition.Longitude}, 'destination_airport', {flight.DestinationAirport.GPSPosition.Latitude}, {flight.DestinationAirport.GPSPosition.Longitude})" +
+                   $"ON CONFLICT(flight_name) DO UPDATE SET fuel_consumption_per_distance = {flight.FuelConsumptionPerDistance},fuel_consumption_takeoff_effort={flight.FuelConsumptionTakeoffEffort}," +
+                    $"departure_airport_latitude={flight.DepartureAirport.GPSPosition.Latitude}, departure_airport_longitude={flight.DepartureAirport.GPSPosition.Longitude}," +
+                    $"destination_airport_latitude={flight.DepartureAirport.GPSPosition.Latitude}, destination_airport_longitude={flight.DestinationAirport.GPSPosition.Longitude};";
                 command.ExecuteNonQuery();
             }
-
-            throw new NotImplementedException();
         }
 
         public void InitializeTables()
         {
             SQLiteConnection.CreateFile("TUI.SimulatorFlights.sqlite");
-            using (var connection = new SQLiteConnection(_connectionString))
+            using (var connection = new SQLiteConnection(@"Data Source=TUI.SimulatorFlights.sqlite;Version=3;"))
             using (var command = connection.CreateCommand())
             {
                 connection.Open();
                 command.CommandText = 
-                    "CREATE TABLE flight (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    "flight_name VARCHAR(255), " +
+                    "CREATE TABLE flight (flight_name VARCHAR(255) PRIMARY KEY, " +
                     "fuel_consumption_per_distance INT," +
                     "fuel_consumption_takeoff_effort INT," +
                     "departure_airport_name VARCHAR(255)," +
