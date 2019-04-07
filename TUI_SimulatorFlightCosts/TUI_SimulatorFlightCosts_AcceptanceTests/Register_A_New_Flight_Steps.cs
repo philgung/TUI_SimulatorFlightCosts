51b17bf74e2078ac11f7e8518fea0728e51c5790
Philippe Gung <philippe.gung@youscribe.com>
2019-04-07 08:58:47 +0200

    fix tests

--- TUI_SimulatorFlightCosts/TUI_SimulatorFlightCosts_AcceptanceTests/Register_A_New_Flight_Steps.cs
+++ TUI_SimulatorFlightCosts/TUI_SimulatorFlightCosts_AcceptanceTests/Register_A_New_Flight_Steps.cs
@@ -1,21 +1,30 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using TechTalk.SpecFlow;
+using TUI.Domain.SimulatorFlights;
+using TUI.SimulatorFlights.Infrastructure;
 
 namespace TUI_SimulatorFlightCosts_AcceptanceTests
 {
     [Binding]
     public class Register_A_New_FlightSteps
     {
+        private ControlTower _currentControlTower = new ControlTower(new SQLLitePersistenceService());
+
         [When(@"A user enters a new flight called '(.*)' on system")]
         public void WhenAUserEntersANewFlightCalledOnSystem(string flightName)
         {
-            ScenarioContext.Current.Pending();
+            var flight = new Flight(flightName);
+            flight.RegisterDepartureAirport("departure_airport", 0.0, 0.0);
+            flight.RegisterDestinationAirport("destination_airport", 0.0, 0.0);
+            _currentControlTower.RegisterFlight(flight);
         }
 
         [Then(@"The flight called '(.*)' can be retrived on system")]
         public void ThenTheFlightCalledCanBeRetrivedOnSystem(string flightName)
         {
-            ScenarioContext.Current.Pending();
+            var flightFound = _currentControlTower.GetFlight(flightName);
+            flightFound.Should().NotBeNull();
         }
     }
 }
