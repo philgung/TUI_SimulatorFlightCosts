263de29abce0aa67026d5a9e1be0e6f8d4ab7a69
Philippe Gung <philippe.gung@youscribe.com>
2019-04-07 09:20:35 +0200

    ajout site mvc init

---
+++ TUI_SimulatorFlightCosts/Presentation/TUI_SimulatorFlightCosts.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TUI_SimulatorFlightCosts.Web.Models;
+
+namespace TUI_SimulatorFlightCosts.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+       
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
