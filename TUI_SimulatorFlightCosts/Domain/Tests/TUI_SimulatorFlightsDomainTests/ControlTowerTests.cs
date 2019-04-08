8c802a071dd50970c6c0afa6fb395f913f98d9f4
Philippe Gung <philippe.gung@youscribe.com>
2019-04-08 17:37:06 +0200

    ajout tests unitaires

--- TUI_SimulatorFlightCosts/Domain/Tests/TUI_SimulatorFlightsDomainTests/ControlTowerTests.cs
+++ TUI_SimulatorFlightCosts/Domain/Tests/TUI_SimulatorFlightsDomainTests/ControlTowerTests.cs
@@ -13,15 +13,16 @@ namespace TUI_SimulatorFlightCostsTests
         const string flightName = "Roissy Charles De Gaulle - London";
         ControlTower _controlTower;
         List<IFlight> _flights = new List<IFlight>();
+        Mock<IPersistenceService> _persistenceServiceMock;
 
         public ControlTowerTests()
         {
-            var persistenceServiceMock = new Mock<IPersistenceService>();
-            persistenceServiceMock.Setup(x => x.SaveFlight(It.IsAny<IFlight>()))
+            _persistenceServiceMock = new Mock<IPersistenceService>();
+            _persistenceServiceMock.Setup(x => x.SaveFlight(It.IsAny<IFlight>()))
                 .Callback((IFlight flight) => _flights.Add(flight));
-            persistenceServiceMock.Setup(x => x.GetFlight(It.IsAny<string>()))
+            _persistenceServiceMock.Setup(x => x.GetFlight(It.IsAny<string>()))
                 .Returns((string flightName) => _flights.SingleOrDefault(x => x.Name == flightName));
-            _controlTower = new ControlTower(persistenceServiceMock.Object);
+            _controlTower = new ControlTower(_persistenceServiceMock.Object);
         }
 
         [Fact]
@@ -62,6 +63,28 @@ namespace TUI_SimulatorFlightCostsTests
             _controlTower.CalculateFuelConsumption(flightName);
             // Assert
             flightMock.Verify(x => x.CalculateFuelConsumption(), Times.Once);
+        }
+
+        [Fact]
+        public void GetReports()
+        {
+            // Arrange
+
+            // Act
+            var reports = _controlTower.GetReports();
+            // Assert
+            _persistenceServiceMock.Verify(x => x.GetReports(), Times.Once);
+        }
+
+        [Fact]
+        public void GetFlights()
+        {
+            // Arrange
+
+            // Act
+            var flights = _controlTower.GetFlights();
+            // Assert
+            _persistenceServiceMock.Verify(x => x.GetFlights(), Times.Once);
         }
     }
 }
